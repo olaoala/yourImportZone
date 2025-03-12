b33aca3ea93d68aeab2b96b0cc24852782eaf4dd
wuraola babalola <babalolawuraola321@gmail.com>
2025-03-12 05:30:25 +0100

    clean code

--- src/Components/CartDetails.jsx
+++ src/Components/CartDetails.jsx
@@ -8,7 +8,7 @@ const [customerEmail, setCustomerEmail] = useState("");
 const [productIds, setProductIds] = useState([]);
 const [isOpen, setIsOpen] = useState(true); // Track modal state
 
-const backendURL = "http://localhost:8888/.netlify/functions/verifyPayment"; // Netlify will proxy this to the function
+const backendURL = ".netlify/functions/verifyPayment"; // Netlify will proxy this to the function
 
 
   console.log(cart, reference, paymentReference, customerEmail, productIds, setPaymentReference )
