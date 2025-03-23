c0893e7713fde3b4024eda83e6abdc671fd838bc
wuraola babalola <babalolawuraola321@gmail.com>
2025-03-23 12:14:58 +0100

    email fix

--- netlify/functions/VerifyPayment.js
+++ netlify/functions/VerifyPayment.js
@@ -62,7 +62,7 @@ exports.handler = async (event) => {
           return null; // Skip invalid products
         }
 
-        const pdfPath = path.join(process.cwd(), "public", "Pdfs", `${productId}.pdf`);
+const pdfPath = path.join(process.cwd(), "public", "Pdfs", `${productIds}.pdf`);
         if (!fs.existsSync(pdfPath)) {
           console.error("PDF not found at:", pdfPath);
           return null;
